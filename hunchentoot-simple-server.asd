dd1dfdee26bf516f6b3e9305ca1a946eacc21a4e
digikar99 <shubhamkarayare@gmail.com>
2022-07-27 09:36:22 +0530

    [add] first commit

---
+++ hunchentoot-simple-server.asd
@@ -0,0 +1,21 @@
+
+
+(defsystem "hunchentoot-simple-server"
+  :depends-on ("hunchentoot"
+               "str"
+               "alexandria"
+               "unix-opts"
+               "swank")
+  :components ((:file "server"))
+  :entry-point "hunchentoot-simple-server:main"
+  :perform (program-op (o c)
+             (dolist (thread (uiop:symbol-call :bt :all-threads))
+               #+sbcl (progn
+                        (require :sb-sprof)
+                        (require :sb-cltl2)
+                        (require :sb-introspect)
+                        (require :sb-bsd-sockets)
+                        (require :sb-posix))
+               (unless (eq thread (uiop:symbol-call :bt :current-thread))
+                 (uiop:symbol-call :bt :destroy-thread thread))
+               (uiop:dump-image "htss" :executable t))))
---
+++ server.lisp
@@ -0,0 +1,79 @@
+
+(defpackage :hunchentoot-simple-server
+  (:use :cl :alexandria :hunchentoot)
+  (:export :main))
+
+(in-package :hunchentoot-simple-server)
+
+(defvar *acceptor*)
+
+(opts:expand-opts
+  (:help "Show this help text")
+  (:no-ssl "Start the server without SSL")
+  (:server-address "<address>:<port> on which to run the server [default: localhost:8000]"
+                   #'identity nil #\s)
+  (:swank-port "Port number at which to start swank" #'identity nil #\p)
+  (:server-root "The root directory from which to serve files (default: current-directory)" #'identity
+                nil #\d))
+
+(defun continue-unless-help (help)
+  (when help
+    (opts:describe :prefix "Usage: htss [options] additional-server-files"
+                   :suffix "additional-server-files: additional lisp files to load")
+    (uiop:quit)))
+
+(defun may-be-start-server (server-address no-ssl server-root)
+  (format t "Will run the server on ~A~%" server-address)
+  (destructuring-bind (address port)
+      (split-sequence:split-sequence #\: server-address)
+    (setq port (parse-integer port))
+    (if no-ssl
+        (setq *acceptor* (make-instance 'hunchentoot:easy-acceptor
+                                        :document-root server-root
+                                        :address address
+                                        :port port))
+        (setq *acceptor* (make-instance 'hunchentoot:easy-ssl-acceptor
+                                        ;; FIXME: Complete SSL
+                                        :document-root server-root
+                                        :address address
+                                        :port port)))
+    (start *acceptor*)))
+
+(defun load-additional-files (files)
+  (when files
+    (loop :for file :in files
+          :do (format t "Loading ~A... " file)
+              (load file)
+              (format t "Done.~%")
+              (finish-output))))
+
+(defun wait-on-hunchentoot-thread ()
+  (bt:join-thread (find-if (lambda (thread)
+                             (str:containsp "hunchentoot-listener" (bt:thread-name thread)))
+                           (bt:all-threads))))
+
+(defun may-be-start-swank (swank-port)
+  (if swank-port
+      (progn
+        (format t "Will start swank on ~A~%" swank-port)
+        (swank:create-server :port (parse-integer swank-port) :dont-close t))
+      (format t "Skipping swank as the port was not supplied~%")) )
+
+(defun main ()
+  (handler-case
+      (multiple-value-bind (parsed-args server-files) (opts:get-opts)
+        (destructuring-bind (&key help
+                               (server-address "localhost:8000")
+                               swank-port
+                               no-ssl
+                               (server-root "./"))
+            parsed-args
+
+          (continue-unless-help help)
+          (may-be-start-server server-address no-ssl server-root)
+          (may-be-start-swank swank-port)
+          (load-additional-files server-files)
+          (wait-on-hunchentoot-thread)))
+    (error (e)
+      (format *error-output* "~A~%" e)
+      (trivial-backtrace:print-backtrace e))))
